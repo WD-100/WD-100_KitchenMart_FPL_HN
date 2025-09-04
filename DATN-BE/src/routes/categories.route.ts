7afe5f34b277950983bd63f5612118d78016008d
thanhtrung10 <dinhthanhtrung1024@gmail.com>
2025-09-04 23:05:07 +0700

    user router

--- DATN-BE/src/routes/categories.route.ts
+++ DATN-BE/src/routes/categories.route.ts
@@ -1,19 +1,8 @@
-import { Router } from "express";
-import { Categories } from "../models/categories.model";
+import {Router} from "express";
+import {list} from "../controllers/category.controller";
 
 const router = Router();
 
-router.get("/list", async (req: any, res: any) => {
-  try {
-    const categories = await Categories.find({ is_deleted: false });
-    res.status(201).json({
-      message: "success",
-      statusCode: 201,
-      data: categories,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
+router.get("/list", list);
 
 export default router;
--- DATN-BE/src/routes/checkout.route.ts
+++ DATN-BE/src/routes/checkout.route.ts
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {checkout, checkout_vnpay, quickOrder} from "../controllers/checkout.controller";
+import {checkout, checkout_vnpay, quickOrder, quickOrderVNPay} from "../controllers/checkout.controller";
 import {authenticateToken} from "../middlewares/auth.middleware";
 
 const router = Router();
@@ -7,6 +7,8 @@ const router = Router();
 router.post("/create", authenticateToken, checkout);
 
 router.post("/quick-order", authenticateToken, quickOrder);
+
+router.post("/quick-order-vnpay", authenticateToken, quickOrderVNPay);
 
 router.post("/checkout_vnpay", authenticateToken, checkout_vnpay);
 
